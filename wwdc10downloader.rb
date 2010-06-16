eaa81f4d25da9f50be7c939a97bccae38a43be1f
Thomas Kollbach <thomas@kollba.ch>
2010-06-16 10:07:28 +0200

    Paralell dl through forks

--- wwdc10downloader.rb
+++ wwdc10downloader.rb
@@ -16,9 +16,9 @@ if ARGV.size < 2
   exit
 end
 
-base_uri = 'https://developer.apple.com/wwdc/scripts/services.php?format=json&type='
+BASE_URI = 'https://developer.apple.com/wwdc/scripts/services.php?format=json&type='
 
-dl_dir = if ARGV.size > 2 
+$dl_dir = if ARGV.size > 2 
   ARGV.last
 else
   'wwdc2010-assets'
@@ -29,27 +29,18 @@ def mkdir(dir)
   Dir.mkdir dir unless File.exists?(dir)
 end
 
-# create dir
-mkdir(dl_dir)
-
-a = Mechanize.new
-
-# Login
-a.get(base_uri + 'docs&session=1') do |page|
-  my_page = page.form_with(:name => 'appleConnectForm') do |f|
-    f.theAccountName  = ARGV[0]
-    f.theAccountPW = ARGV[1]
-  end.click_button
-end
-
-(1..520).each do |session|
+def dl_session(session)
+  cpid = Process.fork
+  
+  return if cpid
+  
   print "Trying session #{session}..."
   
   success = false
   title = nil
   
   # get the title  
-  a.get("#{base_uri}all&session=#{session}") do |page|
+  $a.get("#{BASE_URI}all&session=#{session}") do |page|
     res = JSON.parse(page.body)
     sessions = res['response']['sessions']
     if sessions.size > 0
@@ -63,10 +54,10 @@ end
   
   # get the files
   if success
-    dirname = "#{dl_dir}/#{session}-#{title.gsub(/\/|&|!/, '')}" 
+    dirname = "#{$dl_dir}/#{session}-#{title.gsub(/\/|&|!/, '')}" 
     puts "  Creating #{dirname}"
     mkdir(dirname)
-    a.get("#{base_uri}docs&session=#{session}") do |page|
+    $a.get("#{BASE_URI}docs&session=#{session}") do |page|
       res = JSON.parse(page.body)
       docs = res['response']['docs']
       docs.each do |doc|
@@ -80,7 +71,7 @@ end
               url = (ref_lib == 'iad' ? 'iphone/iad' : ref_lib) + "/library/" + doc_path
               puts "  Downloading #{url}"
               begin
-                a.get("https://developer.apple.com/wwdc/#{url}") do |downloaded_file|
+                $a.get("https://developer.apple.com/wwdc/#{url}") do |downloaded_file|
                   open(dirname + "/" + filename, 'wb') do |file|
                     file.write(downloaded_file.body)
                   end
@@ -94,8 +85,45 @@ end
       end
     end
   end
+
+  exit(0)
 end
 
+# create dir
+mkdir($dl_dir)
+
+$a = Mechanize.new
+
+puts "Login…"
+
+# Login
+$a.get(BASE_URI + 'docs&session=1') do |page|
+  my_page = page.form_with(:name => 'appleConnectForm') do |f|
+    f.theAccountName  = ARGV[0]
+    f.theAccountPW = ARGV[1]
+  end.click_button
+end
+
+$ids_to_dl = (1..520).to_a
+MAX_CHILDREN = 5
+$child_count = 0
+$should_terminate = false
+
+$ids_to_dl.each do |id|
+  if $child_count >= MAX_CHILDREN
+    Process.wait #(-1, Process::WNOHANG)  
+    $child_count -= 1
+    puts "Download for #{id} done"    
+  end
+  
+
+  dl_session(id)
+  $child_count += 1    
+end
+
+
+puts "Waiting for all downloads to complete…"
+Process.waitall
 puts "Done."
 
 
